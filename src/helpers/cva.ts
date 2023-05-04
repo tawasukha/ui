e23c60d3cac3f19c664fe2aa49f627861daf59ef
Forte Zhuo <hedikasmanto@gmail.com>
2023-05-04 07:58:59 +0700

    Checkpoint

--- src/helpers/cva.ts
+++ src/helpers/cva.ts
@@ -1,5 +1,10 @@
-import { type VariantProps as RawVariantProps } from "cva";
-export { cva, cx } from "cva"
+import { type VariantProps as RawVariantProps, cva as _cva, cx as _cx } from "cva";
+import { type ClassValue } from "cva/dist/types";
+import { twMerge as _merge } from "tailwind-merge"
 
+type CVA = typeof _cva
 type RemoveNull<T> = { [P in keyof T]: Partial<NonNullable<T[P]>>; };
 export type VariantProps<T extends (...args: any) => any> = RemoveNull<RawVariantProps<T>>
+
+export const cva: CVA = (base, config) => (props) => _merge(_cva(base, config)(props))
+export const cx = (...inputs: ClassValue[]) => _merge(_cx(inputs)) 
--- src/stories/chip.stories.tsx
+++ src/stories/chip.stories.tsx
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Chip, XChip } from "../components/chip";
+import type { Meta, StoryObj } from "@storybook/react"
+import { Chip, XChip } from "../components/chip"
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta = {
   title: "Tawasukha UI/Chip",
   component: Chip,
   tags: ["docsPage"],
-  argTypes: {
-  },
-} satisfies Meta<typeof Chip>;
+  argTypes: {},
+} satisfies Meta<typeof Chip>
 
-export default meta;
+export default meta
 
 export const Basic: StoryObj<typeof Chip> = {
   args: {
     mode: "success",
     children: "Chip",
+    className: "hover:shadow-none p-10",
   },
   render(args) {
     return <Chip {...args} />
   },
-};
+}
 
 export const Closeable: StoryObj<typeof XChip> = {
   args: {
@@ -30,4 +30,4 @@ export const Closeable: StoryObj<typeof XChip> = {
   render(args) {
     return <XChip {...args} />
   },
-};
+}
